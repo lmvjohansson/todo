6140ce8d269317467492f43791099093304cbcd8
Linnea Johansson <linnea6@kth.se>
2026-04-12 17:31:24 +0200

    Trigger failure mode crash

--- backend/app.py
+++ backend/app.py
@@ -36,7 +36,7 @@ else:
     DB_PORT = os.environ.get('DB_PORT', '5432')
     DB_NAME = os.environ.get('DB_NAME', 'todo_db')
     
-FAILURE_MODE = 'none'
+FAILURE_MODE = 'crash'
 if FAILURE_MODE == 'crash':
     os.kill(os.getppid(), signal.SIGTERM)
 
